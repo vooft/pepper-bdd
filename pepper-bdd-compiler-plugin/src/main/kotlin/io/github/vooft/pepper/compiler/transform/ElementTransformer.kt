233036468d19ce7487a0aabd4bc73b87cd4b7e2c
vooft <52610+vooft@users.noreply.github.com>
2024-08-31 17:54:43 +0400

    wip compiler plugin

--- pepper-bdd-compiler-plugin/src/main/kotlin/io/github/vooft/pepper/compiler/transform/ElementTransformer.kt
+++ pepper-bdd-compiler-plugin/src/main/kotlin/io/github/vooft/pepper/compiler/transform/ElementTransformer.kt
@@ -11,13 +11,15 @@ import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.builders.IrBuilderWithScope
 import org.jetbrains.kotlin.ir.builders.irBlock
 import org.jetbrains.kotlin.ir.builders.irCall
+import org.jetbrains.kotlin.ir.builders.irGet
 import org.jetbrains.kotlin.ir.builders.irReturn
 import org.jetbrains.kotlin.ir.builders.irString
-import org.jetbrains.kotlin.ir.declarations.IrDeclaration
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationParent
+import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.IrFunctionAccessExpression
+import org.jetbrains.kotlin.ir.types.classOrFail
 import org.jetbrains.kotlin.ir.types.typeWith
 import org.jetbrains.kotlin.ir.util.dump
 import org.jetbrains.kotlin.ir.util.hasAnnotation
@@ -32,6 +34,8 @@ internal class ElementTransformer(private val pluginContext: IrPluginContext, pr
     private val symbolGiven = pluginContext.findStep("Given")
     private val symbolWhen = pluginContext.findStep("When")
     private val symbolThen = pluginContext.findStep("Then")
+
+    private val dslClass = pluginContext.referenceClass(ClassId(FqName("io.github.vooft.pepper.dsl"), Name.identifier("PepperSpecDsl")))!!
 
     private val givenContainer = pluginContext.findContainerMethod("GivenContainer")
     private val whenContainer = pluginContext.findContainerMethod("WhenContainer")
@@ -62,7 +66,42 @@ internal class ElementTransformer(private val pluginContext: IrPluginContext, pr
             WHEN -> whenContainer
             THEN -> thenContainer
         }
+
+//        val receiver = pluginContext.referenceClass(ClassId(FqName("io.github.vooft.pepper"), Name.identifier("PepperSpec")))!!
+//            .owner.thisReceiver
+
+        /*
+      CONSTRUCTOR visibility:public <> () returnType:io.github.vooft.pepper.sample.PepperUnprocessedSpec [primary]
+BLOCK_BODY
+  DELEGATING_CONSTRUCTOR_CALL 'public constructor <init> (block: @[ExtensionFunctionType] kotlin.Function1<io.github.vooft.pepper.dsl.PepperSpecDsl, kotlin.Unit>) [primary] declared in io.github.vooft.pepper.PepperSpec'
+    block: FUN_EXPR type=@[ExtensionFunctionType] kotlin.Function1<io.github.vooft.pepper.dsl.PepperSpecDsl, kotlin.Unit> origin=LAMBDA
+      FUN LOCAL_FUNCTION_FOR_LAMBDA name:<anonymous> visibility:local modality:FINAL <> ($receiver:io.github.vooft.pepper.dsl.PepperSpecDsl) returnType:kotlin.Unit
+        $receiver: VALUE_PARAMETER name:<this> type:io.github.vooft.pepper.dsl.PepperSpecDsl
+        BLOCK_BODY
+          VAR name:var1 type:kotlin.String [val]
+            CALL 'public final fun GivenContainer <R> (stepName: kotlin.String, block: kotlin.Function0<R of io.github.vooft.pepper.GivenContainer>): R of io.github.vooft.pepper.GivenContainer declared in io.github.vooft.pepper' type=kotlin.String origin=null
+              <R>: kotlin.String
+              $receiver: GET_VAR '<this>: io.github.vooft.pepper.dsl.PepperSpecDsl declared in io.github.vooft.pepper.sample.PepperUnprocessedSpec.<init>.<anonymous>' type=io.github.vooft.pepper.dsl.PepperSpecDsl origin=null
+              stepName: CONST String type=kotlin.String value="test"
+              block: FUN_EXPR type=kotlin.Function0<kotlin.String> origin=LAMBDA
+                FUN LOCAL_FUNCTION_FOR_LAMBDA name:<anonymous> visibility:local modality:FINAL <> () returnType:kotlin.String
+                  BLOCK_BODY
+                    RETURN type=kotlin.Nothing from='local final fun <anonymous> (): kotlin.String declared in io.github.vooft.pepper.sample.PepperUnprocessedSpec.<init>.<anonymous>'
+                      CALL 'public final fun my test step (): kotlin.String declared in io.github.vooft.pepper.sample' type=kotlin.String origin=null
+  INSTANCE_INITIALIZER_CALL classDescriptor='CLASS CLASS name:PepperUnprocessedSpec modality:FINAL visibility:public superTypes:[io.github.vooft.pepper.PepperSpec]'
+
+         */
+
+        val all = allScopes
+        val reversed = all.reversed()
+        val found = reversed.firstOrNull {
+            val element = it.irElement as? IrSimpleFunction ?: return@firstOrNull false
+            val extension = element.extensionReceiverParameter ?: return@firstOrNull false
+            element.name.asString() == "<anonymous>" && extension.type.classOrFail == dslClass
+        }?.irElement as? IrSimpleFunction ?: error("Cannot find lambda function with PepperSpecDsl receiver")
+
         return irCall(container).apply {
+            this.extensionReceiver = irGet(found.extensionReceiverParameter!!)
             putTypeArgument(0, originalReturnType)
             putValueArgument(0, irString(originalCall.symbol.owner.name.asString()))
             putValueArgument(
@@ -85,8 +124,7 @@ internal class ElementTransformer(private val pluginContext: IrPluginContext, pr
         }
 
         if (expression.symbol.owner.hasAnnotation(stepAnnotation)) {
-            val currentDeclaration = requireNotNull(currentScope?.irElement as? IrDeclaration)
-            return DeclarationIrBuilder(pluginContext, expression.symbol).wrapWithStep(expression, currentDeclaration.parent)
+            return DeclarationIrBuilder(pluginContext, expression.symbol).wrapWithStep(expression, requireNotNull(currentDeclarationParent))
         }
 
         return super.visitCall(expression)
@@ -131,9 +169,18 @@ private fun IrPluginContext.findStep(name: String) = requireNotNull(
     ).single().owner.getter
 ).symbol
 
-private fun IrPluginContext.findContainerMethod(name: String) = referenceFunctions(
-    callableId = CallableId(
-        packageName = FqName("io.github.vooft.pepper.dsl"),
-        callableName = Name.identifier(name)
-    )
-).single().owner
+//private fun IrPluginContext.findContainerMethod(name: String) = referenceFunctions(
+//    callableId = CallableId(
+//        packageName = FqName("io.github.vooft.pepper"),
+//        callableName = Name.identifier("PepperSpec.$name")
+//    )
+//).single().owner
+
+private fun IrPluginContext.findContainerMethod(name: String) = run {
+    referenceFunctions(
+        callableId = CallableId(
+            packageName = FqName("io.github.vooft.pepper"),
+            callableName = Name.identifier(name)
+        )
+    ).single().owner
+}
--- pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/dsl/PepperSpecDsl.kt
+++ pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/dsl/PepperSpecDsl.kt
@@ -2,21 +2,6 @@ package io.github.vooft.pepper.dsl
 
 interface PepperSpecDsl
 
-fun <R> GivenContainer(stepName: String, block: () -> R): R {
-    println("Given: $stepName")
-    return block()
-}
-
-fun <R> WhenContainer(stepName: String, block: () -> R): R {
-    println("When: $stepName")
-    return block()
-}
-
-fun <R> ThenContainer(stepName: String, block: () -> R): R {
-    println("Then: $stepName")
-    return block()
-}
-
 val PepperSpecDsl.Given: Unit get() = pepperFail()
 val PepperSpecDsl.When: Unit get() = pepperFail()
 val PepperSpecDsl.Then: Unit get() = pepperFail()
