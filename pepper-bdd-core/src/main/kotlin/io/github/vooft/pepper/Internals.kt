54b3eca5dbbebe374603288bcc886e4ba96ef787
vooft <52610+vooft@users.noreply.github.com>
2024-09-03 21:41:43 +0400

    wip compiler plugin

--- pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/Internals.kt
+++ pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/Internals.kt
@@ -1,6 +1,14 @@
 package io.github.vooft.pepper
 
 import io.github.vooft.pepper.dsl.ScenarioDsl
+import io.kotest.common.KotestInternal
+import io.kotest.core.names.TestName
+import io.kotest.core.source.sourceRef
+import io.kotest.core.test.NestedTest
+import io.kotest.core.test.TestType.Test
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.coroutineContext
 
 internal suspend fun <R> ScenarioDsl.GivenContainer(id: String, stepName: String, block: suspend () -> R): R =
     testContainer(id, "Given", stepName, block)
@@ -13,3 +21,43 @@ internal suspend fun <R> ScenarioDsl.ThenContainer(id: String, stepName: String,
 
 internal suspend fun <R> ScenarioDsl.AndContainer(id: String, stepName: String, block: suspend () -> R): R =
     testContainer(id, "And", stepName, block)
+
+@OptIn(KotestInternal::class)
+private suspend fun <R> testContainer(id: String, prefix: String, stepName: String, testBlock: suspend () -> R): R {
+    println("$id: $prefix: $stepName")
+
+    val currentScope = requireNotNull(coroutineContext[CurrentTestScope]) { "Test scope is missing in the context" }.scope
+    lateinit var result: StepResult<R>
+
+    currentScope.registerTestCase(
+        NestedTest(
+            name = TestName("$prefix: $stepName"),
+            disabled = false,
+            config = null,
+            type = Test,
+            source = sourceRef()
+        ) {
+            withContext(CoroutineName("step: $stepName")) {
+                result = try {
+                    StepResult.Success(testBlock())
+                } catch (t: Throwable) {
+                    StepResult.Error(t)
+                }
+
+                result.value
+            }
+        }
+    )
+
+    return result.value
+}
+
+private sealed class StepResult<R> {
+    abstract val value: R
+
+    data class Success<R>(override val value: R) : StepResult<R>()
+    data class Error<R>(val error: Throwable) : StepResult<R>() {
+        override val value: R
+            get() = throw error
+    }
+}
--- pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/PepperSpec.kt
+++ pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/PepperSpec.kt
@@ -3,19 +3,13 @@ package io.github.vooft.pepper
 import io.github.vooft.pepper.dsl.PepperSpecDsl
 import io.github.vooft.pepper.dsl.PepperSpecDslImpl
 import io.github.vooft.pepper.dsl.Scenario
-import io.kotest.common.KotestInternal
 import io.kotest.core.names.TestName
-import io.kotest.core.source.sourceRef
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.spec.style.scopes.addContainer
-import io.kotest.core.test.NestedTest
 import io.kotest.core.test.TestScope
-import io.kotest.core.test.TestType.Test
-import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.withContext
 import kotlin.coroutines.AbstractCoroutineContextElement
 import kotlin.coroutines.CoroutineContext
-import kotlin.coroutines.coroutineContext
 
 open class PepperSpec(scenarioBlock: PepperSpecDsl.() -> Scenario) : FunSpec() {
     init {
@@ -32,46 +26,6 @@ data class CurrentTestScope(val scope: TestScope) : AbstractCoroutineContextElem
     companion object Key : CoroutineContext.Key<CurrentTestScope>
 
     override fun toString(): String = "CurrentTestScope($scope)"
-}
-
-@OptIn(KotestInternal::class)
-internal suspend fun <R> testContainer(id: String, prefix: String, stepName: String, testBlock: suspend () -> R): R {
-    println("$id: $prefix: $stepName")
-
-    val currentScope = requireNotNull(coroutineContext[CurrentTestScope]) { "Test scope is missing in the context" }.scope
-    lateinit var result: StepResult<R>
-
-    currentScope.registerTestCase(
-        NestedTest(
-            name = TestName("$prefix: $stepName"),
-            disabled = false,
-            config = null,
-            type = Test,
-            source = sourceRef()
-        ) {
-            withContext(CoroutineName("step: $stepName")) {
-                result = try {
-                    StepResult.Success(testBlock())
-                } catch (t: Throwable) {
-                    StepResult.Error(t)
-                }
-
-                result.value
-            }
-        }
-    )
-
-    return result.value
-}
-
-sealed class StepResult<R> {
-    abstract val value: R
-
-    data class Success<R>(override val value: R) : StepResult<R>()
-    data class Error<R>(val error: Throwable) : StepResult<R>() {
-        override val value: R
-            get() = throw error
-    }
 }
 
 @Target(AnnotationTarget.FUNCTION)
