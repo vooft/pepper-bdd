e647133e0e4b5a883101c351d0a3e7ddc196876f
vooft <52610+vooft@users.noreply.github.com>
2024-09-03 11:50:04 +0400

    wip create dsl

--- pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/PepperSpec.kt
+++ pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/PepperSpec.kt
@@ -1,33 +1,76 @@
 package io.github.vooft.pepper
 
 import io.github.vooft.pepper.dsl.PepperSpecDsl
+import io.kotest.core.names.TestName
+import io.kotest.core.source.sourceRef
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.scopes.addContainer
+import io.kotest.core.test.NestedTest
+import io.kotest.core.test.TestScope
+import io.kotest.core.test.TestType.Test
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.AbstractCoroutineContextElement
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
 
-open class PepperSpec(block: PepperSpecDsl.() -> Unit) : PepperSpecDsl {
+open class PepperSpec(block: suspend PepperSpecDsl.() -> Unit) : PepperSpecDsl, FunSpec() {
     init {
-        block()
+        addContainer(TestName("root"), false, null) {
+            withContext(CurrentTestScope(this)) {
+                block()
+            }
+        }
     }
 
-    internal fun <R> givenContainer(stepName: String, block: () -> R): R {
-        println("Given: $stepName")
-        return block()
+    internal suspend fun <R> givenContainer(stepName: String, block: () -> R): R {
+        return testContainer("Given", stepName, block)
     }
 
-    internal fun <R> whenContainer(stepName: String, block: () -> R): R {
-        println("When: $stepName")
-        return block()
+    internal suspend fun <R> whenContainer(stepName: String, block: () -> R): R {
+        return testContainer("When", stepName, block)
     }
 
-    internal fun <R> thenContainer(stepName: String, block: () -> R): R {
-        println("Then: $stepName")
-        return block()
+    internal suspend fun <R> thenContainer(stepName: String, block: () -> R): R {
+        return testContainer("Then", stepName, block)
     }
+
+    private suspend fun <R> testContainer(prefix: String, stepName: String, block: () -> R): R {
+        println("$prefix: $stepName")
+
+        val currentScope = coroutineContext[CurrentTestScope]!!.scope
+        lateinit var capturedValue: CapturedValue<R>
+
+        currentScope.registerTestCase(NestedTest(
+            name = TestName("$prefix: ", stepName, true),
+            disabled = false,
+            config = null,
+            type = Test,
+            source = sourceRef()
+        ) {
+            capturedValue = CapturedValue(block())
+        })
+
+        return capturedValue.value
+    }
+
+    class CapturedValue<T>(val value: T)
+
 }
 
-fun <R> PepperSpecDsl.GivenContainer(stepName: String, block: () -> R): R = (this as PepperSpec).givenContainer(stepName, block)
+data class CurrentTestScope(val scope: TestScope) : AbstractCoroutineContextElement(CurrentTestScope) {
+    companion object Key : CoroutineContext.Key<CurrentTestScope>
 
-internal fun <R> PepperSpecDsl.WhenContainer(stepName: String, block: () -> R): R = (this as PepperSpec).whenContainer(stepName, block)
+    override fun toString(): String = "CurrentTestScope($scope)"
+}
 
-internal fun <R> PepperSpecDsl.ThenContainer(stepName: String, block: () -> R): R = (this as PepperSpec).thenContainer(stepName, block)
+internal suspend fun <R> PepperSpecDsl.GivenContainer(stepName: String, block: () -> R): R =
+    (this as PepperSpec).givenContainer(stepName, block)
+
+internal suspend fun <R> PepperSpecDsl.WhenContainer(stepName: String, block: () -> R): R =
+    (this as PepperSpec).whenContainer(stepName, block)
+
+internal suspend fun <R> PepperSpecDsl.ThenContainer(stepName: String, block: () -> R): R =
+    (this as PepperSpec).thenContainer(stepName, block)
 
 @Target(AnnotationTarget.FUNCTION)
 annotation class Step
--- pepper-bdd-sample/src/test/kotlin/io/github/vooft/pepper/sample/PepperUnprocessedSpec.kt
+++ pepper-bdd-sample/src/test/kotlin/io/github/vooft/pepper/sample/PepperUnprocessedSpec.kt
@@ -5,17 +5,16 @@ import io.github.vooft.pepper.dsl.Given
 import io.github.vooft.pepper.dsl.Then
 import io.github.vooft.pepper.dsl.When
 
-class PepperUnprocessedSpec :
-    PepperSpec({
-        Given
-        val var1 = `my test step`()
+class PepperUnprocessedSpec : PepperSpec({
+    Given
+    val var1 = `my test step`()
 
-        When
-        val var2 = `my test step 2`(var1)
+    When
+    val var2 = `my test step 2`(var1)
 
-        Then
-        `my test step 3`(var1, var2)
-    })
+    Then
+    `my test step 3`(var1, var2)
+})
 
 // class PepperProcessedSpec : PepperSpec({
 //    GivenContainer { `my test step`() }
