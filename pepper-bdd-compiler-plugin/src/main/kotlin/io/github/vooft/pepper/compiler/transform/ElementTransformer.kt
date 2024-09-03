4400860e32f467534520645781b0a19dc34e277a
vooft <52610+vooft@users.noreply.github.com>
2024-09-03 12:07:14 +0500

    Improve compiler plugin (#2)

--- pepper-bdd-compiler-plugin/src/main/kotlin/io/github/vooft/pepper/compiler/transform/ElementTransformer.kt
+++ pepper-bdd-compiler-plugin/src/main/kotlin/io/github/vooft/pepper/compiler/transform/ElementTransformer.kt
@@ -11,13 +11,15 @@ import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.builders.IrBuilderWithScope
 import org.jetbrains.kotlin.ir.builders.irBlock
 import org.jetbrains.kotlin.ir.builders.irCall
+import org.jetbrains.kotlin.ir.builders.irGet
 import org.jetbrains.kotlin.ir.builders.irReturn
 import org.jetbrains.kotlin.ir.builders.irString
-import org.jetbrains.kotlin.ir.declarations.IrDeclaration
 import org.jetbrains.kotlin.ir.declarations.IrDeclarationParent
+import org.jetbrains.kotlin.ir.declarations.IrSimpleFunction
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrExpression
 import org.jetbrains.kotlin.ir.expressions.IrFunctionAccessExpression
+import org.jetbrains.kotlin.ir.types.classOrFail
 import org.jetbrains.kotlin.ir.types.typeWith
 import org.jetbrains.kotlin.ir.util.dump
 import org.jetbrains.kotlin.ir.util.hasAnnotation
@@ -32,6 +34,15 @@ internal class ElementTransformer(private val pluginContext: IrPluginContext, pr
     private val symbolGiven = pluginContext.findStep("Given")
     private val symbolWhen = pluginContext.findStep("When")
     private val symbolThen = pluginContext.findStep("Then")
+
+    private val dslClass = requireNotNull(
+        pluginContext.referenceClass(
+            ClassId(
+                packageFqName = FqName("io.github.vooft.pepper.dsl"),
+                topLevelName = Name.identifier("PepperSpecDsl")
+            )
+        )
+    )
 
     private val givenContainer = pluginContext.findContainerMethod("GivenContainer")
     private val whenContainer = pluginContext.findContainerMethod("WhenContainer")
@@ -48,7 +59,7 @@ internal class ElementTransformer(private val pluginContext: IrPluginContext, pr
 
     private var currentStep: StepType = GIVEN
 
-    fun IrBuilderWithScope.wrapWithStep(originalCall: IrCall, currentDeclarationParent: IrDeclarationParent): IrFunctionAccessExpression {
+    private fun IrBuilderWithScope.wrapWithStep(originalCall: IrCall, currentDeclarationParent: IrDeclarationParent): IrFunctionAccessExpression {
         val originalReturnType = originalCall.symbol.owner.returnType
 
         val lambda = irLambda(
@@ -62,7 +73,15 @@ internal class ElementTransformer(private val pluginContext: IrPluginContext, pr
             WHEN -> whenContainer
             THEN -> thenContainer
         }
+
+        val found = allScopes.reversed().firstOrNull {
+            val element = it.irElement as? IrSimpleFunction ?: return@firstOrNull false
+            val extension = element.extensionReceiverParameter ?: return@firstOrNull false
+            element.name.asString() == "<anonymous>" && extension.type.classOrFail == dslClass
+        }?.irElement as? IrSimpleFunction ?: error("Cannot find lambda function with PepperSpecDsl receiver")
+
         return irCall(container).apply {
+            this.extensionReceiver = irGet(requireNotNull(found.extensionReceiverParameter))
             putTypeArgument(0, originalReturnType)
             putValueArgument(0, irString(originalCall.symbol.owner.name.asString()))
             putValueArgument(
@@ -85,8 +104,7 @@ internal class ElementTransformer(private val pluginContext: IrPluginContext, pr
         }
 
         if (expression.symbol.owner.hasAnnotation(stepAnnotation)) {
-            val currentDeclaration = requireNotNull(currentScope?.irElement as? IrDeclaration)
-            return DeclarationIrBuilder(pluginContext, expression.symbol).wrapWithStep(expression, currentDeclaration.parent)
+            return DeclarationIrBuilder(pluginContext, expression.symbol).wrapWithStep(expression, requireNotNull(currentDeclarationParent))
         }
 
         return super.visitCall(expression)
@@ -131,9 +149,11 @@ private fun IrPluginContext.findStep(name: String) = requireNotNull(
     ).single().owner.getter
 ).symbol
 
-private fun IrPluginContext.findContainerMethod(name: String) = referenceFunctions(
-    callableId = CallableId(
-        packageName = FqName("io.github.vooft.pepper.dsl"),
-        callableName = Name.identifier(name)
-    )
-).single().owner
+private fun IrPluginContext.findContainerMethod(name: String) = run {
+    referenceFunctions(
+        callableId = CallableId(
+            packageName = FqName("io.github.vooft.pepper"),
+            callableName = Name.identifier(name)
+        )
+    ).single().owner
+}
--- pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/PepperSpec.kt
+++ pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/PepperSpec.kt
@@ -2,12 +2,32 @@ package io.github.vooft.pepper
 
 import io.github.vooft.pepper.dsl.PepperSpecDsl
 
-open class PepperSpec(block: PepperSpecDsl.() -> Unit) {
+open class PepperSpec(block: PepperSpecDsl.() -> Unit) : PepperSpecDsl {
     init {
-        val a = object : PepperSpecDsl {}
-        a.block()
+        block()
+    }
+
+    internal fun <R> givenContainer(stepName: String, block: () -> R): R {
+        println("Given: $stepName")
+        return block()
+    }
+
+    internal fun <R> whenContainer(stepName: String, block: () -> R): R {
+        println("When: $stepName")
+        return block()
+    }
+
+    internal fun <R> thenContainer(stepName: String, block: () -> R): R {
+        println("Then: $stepName")
+        return block()
     }
 }
+
+fun <R> PepperSpecDsl.GivenContainer(stepName: String, block: () -> R): R = (this as PepperSpec).givenContainer(stepName, block)
+
+internal fun <R> PepperSpecDsl.WhenContainer(stepName: String, block: () -> R): R = (this as PepperSpec).whenContainer(stepName, block)
+
+internal fun <R> PepperSpecDsl.ThenContainer(stepName: String, block: () -> R): R = (this as PepperSpec).thenContainer(stepName, block)
 
 @Target(AnnotationTarget.FUNCTION)
 annotation class Step
