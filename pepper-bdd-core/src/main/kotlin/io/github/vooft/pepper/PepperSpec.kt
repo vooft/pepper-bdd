b38dfd8e84ff543bcada05c8e0f9d37e4637fa06
vooft <52610+vooft@users.noreply.github.com>
2024-09-04 19:06:21 +0400

    Improve documentation

--- pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/PepperSpec.kt
+++ pepper-bdd-core/src/main/kotlin/io/github/vooft/pepper/PepperSpec.kt
@@ -3,10 +3,15 @@ package io.github.vooft.pepper
 import io.github.vooft.pepper.dsl.PepperSpecDsl
 import io.github.vooft.pepper.dsl.PepperSpecDslImpl
 import io.github.vooft.pepper.dsl.Scenario
+import io.kotest.common.KotestInternal
 import io.kotest.core.names.TestName
+import io.kotest.core.source.sourceRef
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.core.spec.style.scopes.addContainer
+import io.kotest.core.test.NestedTest
+import io.kotest.core.test.TestType.Test
 import kotlinx.coroutines.withContext
+
 
 open class PepperSpec(scenarioBlock: PepperSpecDsl.() -> Scenario) : FunSpec() {
     init {
@@ -17,6 +22,18 @@ open class PepperSpec(scenarioBlock: PepperSpecDsl.() -> Scenario) : FunSpec() {
         addContainer(TestName("Scenario: ${scenario.name}"), false, null) {
             withContext(CurrentTestScope(this) + RemainingSteps(remainingSteps)) {
                 scenario.scenarioBody()
+            }
+
+            if (remainingSteps.isEmpty()) {
+                @OptIn(KotestInternal::class)
+                registerTestCase(NestedTest(
+                    name = TestName("<no steps found>"),
+                    disabled = false,
+                    config = null,
+                    type = Test,
+                    source = sourceRef(),
+                    test = { throw AssertionError("No steps found, did you register pepper-bdd plugin correctly?") }
+                ))
             }
         }
     }
