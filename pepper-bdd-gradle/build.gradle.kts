2c40f379e5b827b7f67e1b7e228ab5428eb61d9a
vooft <52610+vooft@users.noreply.github.com>
2024-11-02 12:58:57 +0400

    Add publishing to plugins.gradle.org

--- pepper-bdd-gradle/build.gradle.kts
+++ pepper-bdd-gradle/build.gradle.kts
@@ -1,15 +1,19 @@
 plugins {
     `pepper-bdd-base`
-    `pepper-bdd-publish`
-    id("java-gradle-plugin")
     alias(libs.plugins.buildconfig)
+    alias(libs.plugins.plugin.publish)
 }
 
 gradlePlugin {
+    website.set("https://github.com/vooft/pepper-bdd")
+    vcsUrl.set("https://github.com/vooft/pepper-bdd")
     plugins {
         create("pepperPlugin") {
             id = "io.github.vooft.pepper-bdd"
             implementationClass = "io.github.vooft.pepper.gradle.PepperBddGradleSubPlugin"
+            displayName = "Pepper BDD plugin"
+            description = "Pepper BDD testing library for Kotlin Gradle plugin"
+            tags.set(listOf("bdd", "kotlin", "testing", "kotest"))
         }
     }
 }
