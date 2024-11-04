8abe8e20d6785c31ba3320de13d0dd0be00759a7
vooft <52610+vooft@users.noreply.github.com>
2024-11-04 22:14:09 +0400

    WIP reporting

--- pepper-bdd-reports/pepper-bdd-reports-api/build.gradle.kts
+++ pepper-bdd-reports/pepper-bdd-reports-api/build.gradle.kts
@@ -1,7 +1,10 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 
 plugins {
     kotlin("multiplatform")
+    alias(libs.plugins.kotest.multiplatform)
     alias(libs.plugins.kotlinx.serialization)
     `pepper-bdd-base`
     `pepper-bdd-publish`
@@ -21,10 +24,23 @@ kotlin {
             api(libs.kotlinx.datetime)
             api(libs.kotlinx.serialization.json)
         }
-        jvmTest.dependencies {
-            implementation(kotlin("reflect"))
-            implementation(libs.kotest.runner.jvm)
+        commonTest.dependencies {
+            implementation(libs.kotest.framework.engine)
             implementation(libs.kotest.assertions.json)
         }
+        jvmTest.dependencies {
+            implementation(libs.kotest.runner.jvm)
+            implementation(kotlin("reflect"))
+        }
+    }
+}
+
+tasks.named<Test>("jvmTest") {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        showStandardStreams = true
+        showExceptions = true
     }
 }
---
+++ pepper-bdd-reports/pepper-bdd-reports-api/src/commonTest/kotlin/api/PepperTestProjectTest.kt
@@ -0,0 +1,91 @@
+package api
+
+import io.github.pepper.reports.api.PepperScenarioStatus.PASSED
+import io.github.pepper.reports.api.PepperTestProject
+import io.github.pepper.reports.api.PepperTestScenario
+import io.github.pepper.reports.api.PepperTestStep
+import io.kotest.assertions.json.shouldEqualJson
+import io.kotest.core.spec.style.ShouldSpec
+import kotlinx.datetime.Instant
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class PepperTestProjectTest : ShouldSpec({
+    should("serialize PepperTestProject") {
+        val project = PepperTestProject(
+            scenarios = listOf(
+                PepperTestScenario(
+                    className = "io.github.pepper.reports.api.PepperTestProjectTest",
+                    name = "serialize PepperTestProject",
+                    steps = listOf(
+                        PepperTestStep(
+                            name = "create PepperTestProject",
+                            error = null,
+                            startedAt = Instant.parse("2021-08-01T00:00:00Z"),
+                            finishedAt = Instant.parse("2021-08-01T00:00:01Z"),
+                        ),
+                        PepperTestStep(
+                            name = "serialize PepperTestProject",
+                            error = null,
+                            startedAt = Instant.parse("2021-09-01T00:00:00Z"),
+                            finishedAt = Instant.parse("2021-09-01T00:00:01Z"),
+                        ),
+                        PepperTestStep(
+                            name = "assert PepperTestProject",
+                            error = """
+                                java.lang.AssertionError: boohoo
+                                    at io.github.pepper.reports.api.PepperTestProjectTest.serialize PepperTestProject(PepperTestProjectTest.kt:42)
+                                """.trimIndent(),
+                            startedAt = Instant.parse("2021-10-01T00:00:00Z"),
+                            finishedAt = Instant.parse("2021-10-01T00:00:01Z"),
+                        )
+                    ),
+                    status = PASSED,
+                    startedAt = Instant.parse("2021-08-01T00:00:00Z"),
+                    finishedAt = Instant.parse("2021-08-01T00:00:01Z"),
+                )
+            ),
+            startedAt = Instant.parse("2021-08-01T00:00:00Z"),
+            finishedAt = Instant.parse("2021-08-01T00:00:01Z"),
+        )
+
+        val json = Json.encodeToString(project)
+
+        // language=JSON
+        json shouldEqualJson """
+            {
+              "scenarios": [
+                {
+                  "className": "io.github.pepper.reports.api.PepperTestProjectTest",
+                  "name": "serialize PepperTestProject",
+                  "steps": [
+                    {
+                      "name": "create PepperTestProject",
+                      "error": null,
+                      "startedAt": "2021-08-01T00:00:00Z",
+                      "finishedAt": "2021-08-01T00:00:01Z"
+                    },
+                    {
+                      "name": "serialize PepperTestProject",
+                      "error": null,
+                      "startedAt": "2021-09-01T00:00:00Z",
+                      "finishedAt": "2021-09-01T00:00:01Z"
+                    },
+                    {
+                      "name": "assert PepperTestProject",
+                      "error": "java.lang.AssertionError: boohoo\n    at io.github.pepper.reports.api.PepperTestProjectTest.serialize PepperTestProject(PepperTestProjectTest.kt:42)",
+                      "startedAt": "2021-10-01T00:00:00Z",
+                      "finishedAt": "2021-10-01T00:00:01Z"
+                    }
+                  ],
+                  "status": "PASSED",
+                  "startedAt": "2021-08-01T00:00:00Z",
+                  "finishedAt": "2021-08-01T00:00:01Z"
+                }
+              ],
+              "startedAt": "2021-08-01T00:00:00Z",
+              "finishedAt": "2021-08-01T00:00:01Z"
+            }
+            """.trimIndent()
+    }
+})
