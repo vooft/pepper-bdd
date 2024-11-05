b79281eb21c321811305ae756e2d499f8d79c240
vooft <52610+vooft@users.noreply.github.com>
2024-11-05 09:17:46 +0400

    WIP reporting

--- pepper-bdd-reports/pepper-bdd-reports-api/src/commonTest/kotlin/api/PepperTestProjectTest.kt
+++ pepper-bdd-reports/pepper-bdd-reports-api/src/commonTest/kotlin/api/PepperTestProjectTest.kt
@@ -11,66 +11,65 @@ import kotlinx.datetime.Instant
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
-class PepperTestProjectTest :
-    ShouldSpec({
-        should("serialize PepperTestProject") {
-            val project = PepperTestProject(
-                version = 1,
-                scenarios = listOf(
-                    PepperTestScenario(
-                        className = "io.github.pepper.reports.api.PepperTestProjectTest",
-                        name = "serialize PepperTestProject",
-                        steps = listOf(
-                            PepperTestStep(
-                                name = "create PepperTestProject",
-                                arguments = listOf(
-                                    StepArgument(
-                                        name = "number",
-                                        type = "kotlin.Int",
-                                        value = "1"
-                                    )
-                                ),
-                                error = null,
-                                startedAt = Instant.parse("2021-08-01T00:00:00Z"),
-                                finishedAt = Instant.parse("2021-08-01T00:00:01Z"),
+class PepperTestProjectTest : ShouldSpec({
+    should("serialize PepperTestProject") {
+        val project = PepperTestProject(
+            version = 1,
+            scenarios = listOf(
+                PepperTestScenario(
+                    className = "io.github.pepper.reports.api.PepperTestProjectTest",
+                    name = "serialize PepperTestProject",
+                    steps = listOf(
+                        PepperTestStep(
+                            name = "create PepperTestProject",
+                            arguments = listOf(
+                                StepArgument(
+                                    name = "number",
+                                    type = "kotlin.Int",
+                                    value = "1"
+                                )
                             ),
-                            PepperTestStep(
-                                name = "serialize PepperTestProject",
-                                arguments = listOf(
-                                    StepArgument(
-                                        name = "project",
-                                        type = "io.github.pepper.reports.api.PepperTestProject",
-                                        value = "fancy to string implementation"
-                                    )
-                                ),
-                                error = null,
-                                startedAt = Instant.parse("2021-09-01T00:00:00Z"),
-                                finishedAt = Instant.parse("2021-09-01T00:00:01Z"),
+                            error = null,
+                            startedAt = Instant.parse("2021-08-01T00:00:00Z"),
+                            finishedAt = Instant.parse("2021-08-01T00:00:01Z"),
+                        ),
+                        PepperTestStep(
+                            name = "serialize PepperTestProject",
+                            arguments = listOf(
+                                StepArgument(
+                                    name = "project",
+                                    type = "io.github.pepper.reports.api.PepperTestProject",
+                                    value = "fancy to string implementation"
+                                )
                             ),
-                            PepperTestStep(
-                                name = "assert PepperTestProject",
-                                arguments = listOf(),
-                                error = """
+                            error = null,
+                            startedAt = Instant.parse("2021-09-01T00:00:00Z"),
+                            finishedAt = Instant.parse("2021-09-01T00:00:01Z"),
+                        ),
+                        PepperTestStep(
+                            name = "assert PepperTestProject",
+                            arguments = listOf(),
+                            error = """
                                 java.lang.AssertionError: boohoo
                                     at io.github.pepper.reports.api.PepperTestProjectTest.serialize PepperTestProject(PepperTestProjectTest.kt:42)
-                                """.trimIndent(),
-                                startedAt = Instant.parse("2021-10-01T00:00:00Z"),
-                                finishedAt = Instant.parse("2021-10-01T00:00:01Z"),
-                            )
-                        ),
-                        status = PASSED,
-                        startedAt = Instant.parse("2021-08-01T00:00:00Z"),
-                        finishedAt = Instant.parse("2021-08-01T00:00:01Z"),
-                    )
-                ),
-                startedAt = Instant.parse("2021-08-01T00:00:00Z"),
-                finishedAt = Instant.parse("2021-08-01T00:00:01Z"),
-            )
+                            """.trimIndent(),
+                            startedAt = Instant.parse("2021-10-01T00:00:00Z"),
+                            finishedAt = Instant.parse("2021-10-01T00:00:01Z"),
+                        )
+                    ),
+                    status = PASSED,
+                    startedAt = Instant.parse("2021-08-01T00:00:00Z"),
+                    finishedAt = Instant.parse("2021-08-01T00:00:01Z"),
+                )
+            ),
+            startedAt = Instant.parse("2021-08-01T00:00:00Z"),
+            finishedAt = Instant.parse("2021-08-01T00:00:01Z"),
+        )
 
-            val json = Json.encodeToString(project)
+        val json = Json.encodeToString(project)
 
-            // language=JSON
-            json shouldEqualJson """
+        // language=JSON
+        json shouldEqualJson """
             {
               "version": 1,
               "scenarios": [
@@ -120,6 +119,6 @@ class PepperTestProjectTest :
               "startedAt": "2021-08-01T00:00:00Z",
               "finishedAt": "2021-08-01T00:00:01Z"
             }
-            """.trimIndent()
-        }
-    })
+        """.trimIndent()
+    }
+})
