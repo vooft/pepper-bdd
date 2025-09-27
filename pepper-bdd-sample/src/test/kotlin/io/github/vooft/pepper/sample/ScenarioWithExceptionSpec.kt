6ea4322b8d02ba6e9d2cb77bfb1649e2b80a64ae
Sergey Lapin <slaaap@gmail.com>
2025-09-27 10:09:29 +0100

    add failing test

---
+++ pepper-bdd-sample/src/test/kotlin/io/github/vooft/pepper/sample/ScenarioWithExceptionSpec.kt
@@ -0,0 +1,21 @@
+package io.github.vooft.pepper.sample
+
+import io.github.vooft.pepper.PepperSpec
+import io.github.vooft.pepper.dsl.Given
+import io.github.vooft.pepper.dsl.Then
+import io.github.vooft.pepper.dsl.When
+import io.kotest.core.annotation.Ignored
+
+@Ignored
+class ScenarioWithExceptionSpec : PepperSpec({
+    Scenario("my test scenario that should fail") {
+        Given
+        val firstRandom = `generate random string`("first")
+        val secondRandom = `generate random string`("second")
+        When
+        val compareResult = `two strings are compared`(firstRandom, secondRandom)
+        error("YOU SHALL NOT PASS!!")
+        Then
+        `deep nested compare result is '{expected}'`(Holder(Holder(compareResult)), false)
+    }
+})
